d643b9d5201a33f53bfdf4b7bdf66ff5e48d7b4f
Carmen Burim <carmenburimyt@gmail.com>
2026-01-08 22:46:42 -0300

    teste

--- index.js
+++ index.js
@@ -1,67 +1,100 @@
 const express = require('express');
+const cors = require('cors');
+const pool = require('./database');
+
 const app = express();
 const port = 3000;
 
+app.use(cors());
 app.use(express.json());
 
-let todos = [
-  { id: 1, titulo: "comprar ração do pitico", concluido: false },
-  { id: 2, titulo: "comprar pão", concluido: true },
-  { id: 3, titulo: "levar mãe no médico", concluido: false }
-];
-let nextId = 4;
 
-// rota /todos
-app.get('/todos', (req, res) => {
-  res.json(todos);
+app.get('/', (req, res) => {
+  res.send('API To-Do List com MariaDB rodando 🚀');
 });
 
-//rota de post  /todos 
-app.post('/todos', (req, res) => {
-  const { titulo, concluido = false } = req.body;
+
+app.get('/todos', async (req, res) => {
+  try {
+    const conn = await pool.getConnection();
+    const rows = await conn.query('SELECT * FROM todos');
+    conn.release();
+
+    res.json(rows);
+  } catch (error) {
+    res.status(500).json({ erro: 'Erro ao buscar tarefas' });
+  }
+});
+
+
+app.post('/todos', async (req, res) => {
+  const { titulo } = req.body;
 
   if (!titulo) {
     return res.status(400).json({ erro: 'O campo "titulo" é obrigatório.' });
   }
 
-  const novoTodo = {
-    id: nextId++,
-    titulo,
-    concluido
-  };
+  try {
+    const conn = await pool.getConnection();
+    const result = await conn.query(
+      'INSERT INTO todos (titulo) VALUES (?)',
+      [titulo]
+    );
+    conn.release();
 
-  todos.push(novoTodo);
-  res.status(201).json(novoTodo);
+    res.status(201).json({
+      id: result.insertId,
+      titulo,
+      concluido: false
+    });
+  } catch (error) {
+    res.status(500).json({ erro: 'Erro ao criar tarefa' });
+  }
 });
 
-// rota de put /todos/:id 
-app.put('/todos/:id', (req, res) => {
-  const id = parseInt(req.params.id);
+
+app.put('/todos/:id', async (req, res) => {
   const { titulo, concluido } = req.body;
+  const id = req.params.id;
 
-  const todo = todos.find(t => t.id === id);
+  try {
+    const conn = await pool.getConnection();
+    const result = await conn.query(
+      'UPDATE todos SET titulo = ?, concluido = ? WHERE id = ?',
+      [titulo, concluido, id]
+    );
+    conn.release();
 
-  if (!todo) {
-    return res.status(404).json({ erro: 'Tem isso não mano' });
+    if (result.affectedRows === 0) {
+      return res.status(404).json({ erro: 'Tarefa não encontrada' });
+    }
+
+    res.json({ id, titulo, concluido });
+  } catch (error) {
+    res.status(500).json({ erro: 'Erro ao atualizar tarefa' });
   }
-
-  if (titulo !== undefined) todo.titulo = titulo;
-  if (concluido !== undefined) todo.concluido = concluido;
-
-  res.json(todo);
 });
 
-// rota DELETE /todos/:id 
-app.delete('/todos/:id', (req, res) => {
-  const id = parseInt(req.params.id);
-  const index = todos.findIndex(t => t.id === id);
 
-  if (index === -1) {
- return res.status(404).json({ erro: 'Tem isso não mano.' });
+app.delete('/todos/:id', async (req, res) => {
+  const id = req.params.id;
+
+  try {
+    const conn = await pool.getConnection();
+    const result = await conn.query(
+      'DELETE FROM todos WHERE id = ?',
+      [id]
+    );
+    conn.release();
+
+    if (result.affectedRows === 0) {
+      return res.status(404).json({ erro: 'Tarefa não encontrada' });
+    }
+
+    res.json({ mensagem: 'Tarefa removida com sucesso' });
+  } catch (error) {
+    res.status(500).json({ erro: 'Erro ao remover tarefa' });
   }
-
-  const removido = todos.splice(index, 1);
-  res.json(removido[0]);
 });
 
 
